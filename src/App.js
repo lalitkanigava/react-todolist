5b14039a0cebffe942e18fd92d9d9cf88bfb906a
lalitkanigava <lalit.kanigava@gmail.com>
2020-08-18 15:12:01 +0530

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+let getTime = "00:00:00";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let [time, updateTime] = useState(getTime);
+
+	function currentTime() {
+		let time = new Date().toLocaleTimeString();
+
+		updateTime(time);
+	}
+	let temp = setInterval(currentTime, 1000);
+
+	function resetTime() {
+		clearInterval(temp);
+		time = "00:00:00";
+
+		updateTime(time);
+	}
+
+	return (
+		<div className="container">
+			<div className="hero">
+				<h1 className="time">{time}</h1>
+				<div className="buttons">
+					{/* <button className="start" onClick={startTime}>
+						Start Time
+					</button>
+					<button className="reset" onClick={resetTime}>
+						Reset
+					</button> */}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
