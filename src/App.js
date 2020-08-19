eb1d918872bfe0e8f0fd04434ee36a21484339cd
lalitkanigava <lalit.kanigava@gmail.com>
2020-08-19 17:33:30 +0530

    updated CSS

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
 import React, { useState } from "react";
+import Lists from "./components/Lists";
+import { InputGroup, FormControl, Button } from "react-bootstrap";
 import "./App.css";
 
-let getTime = "00:00:00";
-
 function App() {
-	let [time, updateTime] = useState(getTime);
-
-	function currentTime() {
-		let time = new Date().toLocaleTimeString();
-
-		updateTime(time);
-	}
-	let temp = setInterval(currentTime, 1000);
-
-	function resetTime() {
-		clearInterval(temp);
-		time = "00:00:00";
-
-		updateTime(time);
+	let [inputdata, getValue] = useState([]);
+	function setValue(event) {
+		inputdata = event.target.value;
+		getValue(...inputdata);
 	}
 
 	return (
-		<div className="container">
-			<div className="hero">
-				<h1 className="time">{time}</h1>
-				<div className="buttons">
-					{/* <button className="start" onClick={startTime}>
-						Start Time
-					</button>
-					<button className="reset" onClick={resetTime}>
-						Reset
-					</button> */}
+		<>
+			<div className="container">
+				<div className="hero">
+					<h1 className="heading">ToDoList</h1>
+					<div className="input-div">
+						<InputGroup className="mb-3">
+							<FormControl
+								className="w-25"
+								placeholder="Enter task"
+								type="text"
+								onChange={setValue}
+							/>
+						</InputGroup>
+						<Button className="button" variant="success">
+							+
+						</Button>
+					</div>
+					<hr></hr>
+
+					<div>
+						<Lists />
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
---
+++ src/components/Lists.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import "../App.css";
+
+function Lists() {
+	return (
+		<div className="list-data">
+			<p className="delete"> X </p>
+			<h5 className="demo-text">demo</h5>
+		</div>
+	);
+}
+export default Lists;
